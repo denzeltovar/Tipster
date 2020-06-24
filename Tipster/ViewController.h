589bf3210e7a52be37784095da79e42b2d3dc35c
Denzel Tovar <denzeltov@fb.com>
2020-06-24 12:51:22 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by admin on 6/24/20.
+//  Copyright © 2020 Denzel Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipster/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by admin on 6/24/20.
+//  Copyright © 2020 Denzel Tovar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
