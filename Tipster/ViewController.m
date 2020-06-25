680ef4c6cc54d573e540414259f84970907b19d2
Denzel Tovar <denzeltov@fb.com>
2020-06-25 09:27:02 -0700

    Implemented Tip Calculator (MVP)

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -18,6 +22,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percent = @[@(0.15), @(0.20), @(0.22)];
+    
+    double tipPercent = [percent[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip  = tipPercent * bill;
+    double total  = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat: @"$%.2f", tip];
+    
+    self.totalLabel.text = [NSString stringWithFormat: @"$%2.f", total];
+    
+}
+
 
 
 @end
